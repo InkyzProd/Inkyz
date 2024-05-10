242a5403f0dc1371c3e61d652446fefdc7397074
Inkyz <crackteam.conf@gmail.com>
2024-05-10 18:54:56 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+console.log("Y")
+
+function idk() {
+  setTimeout(function() {
+    window.location.href = "404.html";
+  }, 3000);
+};
+
+/*document.getElementById("redirectButton").addEventListener("click", function() {
+    setTimeout(function() {
+        window.location.href = "404.html";
+    }, 3000);
+});*/
